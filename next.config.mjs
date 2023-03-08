eb91b5b818fd07059c2860cc611bb9faa0fb14d5
abdelkee <abdell.anejjar@gmail.com>
2023-03-07 21:47:27 -0500

    feat: add pwa support

--- next.config.mjs
+++ next.config.mjs
@@ -6,19 +6,14 @@
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
 
+import withPWA from "next-pwa";
 /** @type {import("next").NextConfig} */
+
 const config = {
   reactStrictMode: true,
-
-  /**
-   * If you have the "experimental: { appDir: true }" setting enabled, then you
-   * must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
 };
-export default config;
+
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+})(config);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,28 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
+import type { AppType } from "next/app";
+import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 };
 
